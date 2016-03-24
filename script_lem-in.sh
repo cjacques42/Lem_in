3f453b59be59d1bed897c64a81b0bca2d708a87e
Corentin JACQUES <cjacques@e2r1p5.42.fr>
2016-03-24 11:44:33 +0100

    file test

---
+++ script_lem-in.sh
@@ -0,0 +1,40 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    script_lem-in.sh                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cjacques <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2016/03/24 11:09:09 by cjacques          #+#    #+#              #
+#    Updated: 2016/03/24 11:43:21 by cjacques         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+echo "Test 0: "
+./lem-in < test/test0
+echo "Test 1: "
+./lem-in < test/test1
+echo "Test 2: "
+./lem-in < test/test2
+echo "Test 3: "
+./lem-in < test/test3
+echo "Test 4: "
+./lem-in < test/test4
+echo "Test 5: "
+./lem-in < test/test5
+echo "Test 6: "
+./lem-in < test/test6
+echo "Test 7: "
+./lem-in < test/test7
+echo "Test 8: "
+./lem-in < test/test8
+echo "Test 9: "
+./lem-in < test/test9
+echo "Test 10: "
+./lem-in < test/test10
+echo "Test 11: "
+./lem-in < test/test11
+echo "Test 12: "
+./lem-in < test/test12
+echo "Test 13: "
+./lem-in < test/test13
